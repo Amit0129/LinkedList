83e4ced76d472bfc016008b023ab5dbce8e99213
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-14 00:13:40 +0530

    Creating a simple LinkkedList of 56, 30, 70

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("This is UnOrdered LinkedList API Program");
+
+            LinkedList<int> numberLst = new LinkedList<int>();
+            numberLst.AddLast(56);
+            numberLst.AddLast(30);
+            numberLst.AddLast(70);
+
+            foreach (int item in numberLst)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
